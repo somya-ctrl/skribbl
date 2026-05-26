0736a175e4dbd5cbc48e1bc916402756ee1e48b6
somya-ctrl <agarwalsomya224@gmail.com>
2026-05-27 01:19:09 +0530

    backend deployment

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://skribbl-smoky.vercel.app",
     methods: ["GET", "POST"],
   },
 });
@@ -116,6 +116,8 @@ io.on("connection", (socket) => {
 
 });
 
-server.listen(3000, () => {
-  console.log("Server running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
