d6b359a3b6a56066de328abc2efa941456db2a32
somya-ctrl <agarwalsomya224@gmail.com>
2026-05-26 10:15:32 +0530

    room created

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-
+const rooms = require("./rooms");
 const app = express();
 
 app.use(cors());
@@ -17,11 +17,49 @@ const io = new Server(server, {
 });
 
 io.on("connection", (socket) => {
-  console.log("User connected:", socket.id);
 
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
+  socket.on("create_room", ({ playerName }) => {
+  const roomId = Math.random().toString(36).substring(2, 8);
+
+  rooms[roomId] = {
+    players: [
+      {
+        id: socket.id,
+        name: playerName,
+      },
+    ],
+  };
+
+  socket.join(roomId);
+
+  socket.emit("room_created", {
+    roomId,
+    players: rooms[roomId].players,
   });
+
+  console.log("Room created:", roomId);
+});
+  socket.on("join_room", ({ roomId, playerName }) => {
+  const room = rooms[roomId];
+
+  if (!room) {
+    socket.emit("error_message", "Room not found");
+    return;
+  }
+
+  const player = {
+    id: socket.id,
+    name: playerName,
+  };
+
+  room.players.push(player);
+
+  socket.join(roomId);
+
+  io.to(roomId).emit("player_list", room.players);
+
+  console.log(`${playerName} joined ${roomId}`);
+});
 });
 
 server.listen(3000, () => {
